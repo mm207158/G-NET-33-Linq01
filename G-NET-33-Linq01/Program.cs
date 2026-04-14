bfa3c7cdf4a3b826bbd08bdac0ca816f44a20552
mm207158 <divmariam2021@gmail.com>
2026-04-14 20:24:19 +0200

    Add project files.

---
+++ G-NET-33-Linq01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_33_Linq01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
